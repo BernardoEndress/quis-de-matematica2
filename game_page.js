274c024d82e6fcb3b897583e8850da8e5bf2fef2
BernardoEndress <113397256+BernardoEndress@users.noreply.github.com>
2023-02-15 19:36:47 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,6 @@
+player1=localStorage.getItem("player1name");
+player2=localStorage.getItem("player2name");
+player1score=0;
+player2score=0;
+document.getElementById("player1Name").innerHTML=player1+": ";
+document.getElementById("player2Name").innerHTML=player2+": ";
